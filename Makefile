cd1544c56edc6c822ccd8d4afa6c8780775aab65
shurajan <alexander.bralnin@gmail.com>
2020-11-12 23:07:54 +0300

    Version 0.0.2 - minor fix to make

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
-PYTHON=python3
+PYTHON=python
 
 SEARCH=
 .PHONY: clean
+.PHONY: test
 .PHONY: build
 .PHONY: testpypi
+.PHONY: publish
 
 clean:
 	-@rm -rf build/
@@ -11,8 +13,19 @@ clean:
 	-@rm -rf *.egg-info
 	-@rm -rf .pytest_cache/
 
+test:
+	@black .
+	@$(PYTHON) -m pytest tests
+
 build:
 	@$(PYTHON) setup.py sdist bdist_wheel
 
 testpypi:
 	@$(PYTHON) -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+publish:
+	@$(MAKE) clean
+	@$(MAKE) build
+	@$(MAKE) testpypi
+
+
--- tests/test_sbireport.py
+++ tests/test_sbireport.py
@@ -3,14 +3,18 @@ import pandas as pd
 from sbireport.trades import extract_trades
 from sbireport.calculators import calcposition, calcweight
 
-rpt_path = (pathlib.Path.cwd() / ".." / "data").resolve()
-ex1_rpt_path = (pathlib.Path.cwd() / ".." / "data" / "EX-1-REGULAR.html").resolve()
+rpt_path = (pathlib.Path.cwd() / ".." / "sbireport" / "data").resolve()
+ex1_rpt_path = (
+    pathlib.Path.cwd() / ".." / "sbireport" / "data" / "EX-1-REGULAR.html"
+).resolve()
+
 
 def test_trades_extractor_pass_0():
     trades = extract_trades(rpt_path)
     print(trades)
     assert isinstance(trades, pd.DataFrame)
     assert len(trades) == 11
+
 
 def test_trades_extractor_pass_1():
     trades = extract_trades(ex1_rpt_path)
