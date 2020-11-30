ed2071ea191bb69cb60fe094972a074ebd64d9fc
shurajan <alexander.bralnin@gmail.com>
2020-11-30 15:45:29 +0300

    Version 0.0.2 - added comissions to position

--- sbireport/calculators.py
+++ sbireport/calculators.py
@@ -5,22 +5,47 @@ def calcposition(trades: pd.DataFrame, index_field="Код ЦБ") -> pd.DataFram
     buys = (
         trades[trades["Вид"] == "Покупка"]
         .groupby(index_field)
-        .agg({"Количество, шт.": "sum", "Сумма": "sum"})
+        .agg(
+            {
+                "Количество, шт.": "sum",
+                "Сумма": "sum",
+                "Комиссия Брокера": "sum",
+                "Комиссия Биржи": "sum",
+            }
+        )
     )
-    # sells = buys.copy()
+
     buys["Сумма"] = buys["Сумма"].apply(lambda x: -x)
+
     sells = (
         trades[trades["Вид"] == "Продажа"]
         .groupby(index_field)
-        .agg({"Количество, шт.": "sum", "Сумма": "sum"})
+        .agg(
+            {
+                "Количество, шт.": "sum",
+                "Сумма": "sum",
+                "Комиссия Брокера": "sum",
+                "Комиссия Биржи": "sum",
+            }
+        )
     )
 
     sells["Количество, шт."] = sells["Количество, шт."].apply(lambda x: -x)
     position = (
         buys.append(sells)
         .groupby(index_field)
-        .agg({"Количество, шт.": "sum", "Сумма": "sum"})
+        .agg(
+            {
+                "Количество, шт.": "sum",
+                "Сумма": "sum",
+                "Комиссия Брокера": "sum",
+                "Комиссия Биржи": "sum",
+            }
+        )
     )
+
+    position["Комиссия Брокера"] = position["Комиссия Брокера"].apply(lambda x: -x)
+    position["Комиссия Биржи"] = position["Комиссия Биржи"].apply(lambda x: -x)
 
     # position = position.join(static_data, on='Код ЦБ')
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["pandas>=1.1.4", "beautifulsoup4>=4.9.3", "XlsxWriter>=1.3.7"]
 
 setup(
     name="sbireport",
-    version="0.0.1",
+    version="0.0.2",
     author="shurajan",
     author_email="neverbreaks2020@gmail.com",
     description="A package to parse Sberbank Investor html reports",
